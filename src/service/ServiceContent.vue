e03a9df771ec1802056eaf506b0083aecb852712
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2022-12-04 08:17:35 +0000

    modify the service and the portfolio content

--- src/service/ServiceContent.vue
+++ src/service/ServiceContent.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="mx-auto max-w-screen-xl container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-10 lg:px-5">
-    <div class="flex justify-start items-center h-52 px-2 sm:px-10 bg-gray-800 rounded">
-      <figure class="flex flex-col items-center md:items-start">
+    <div class="flex justify-center items-center h-52 px-2 sm:px-10 bg-gray-800 rounded">
+      <figure class="flex flex-col justify-center items-center">
         <internet-icon class="w-9 text-white mb-4 md:mb-0" />
-        <figcaption class="text-white font-bold text-lg md:text-xl">Web App Development</figcaption>
-        <p class="text-white text-center md:text-start">I develop both backend and frontend web applications</p>
+        <figcaption class="text-white font-bold text-center text-lg md:text-xl">Web App Development</figcaption>
+        <p class="text-white text-center">I develop both backend and frontend web applications</p>
       </figure>
     </div>
-    <div class="flex justify-start items-center h-52 px-2 sm:px-10 bg-gray-800 rounded">
-      <figure class="flex flex-col items-center md:items-start">
+    <div class="flex justify-center items-center h-52 px-2 sm:px-10 bg-gray-800 rounded">
+      <figure class="flex flex-col justify-center items-center">
         <phone-icon class="w-9 text-white mb-4 md:mb-0" />
-        <figcaption class="text-white font-bold text-lg md:text-xl">Mobile App Development</figcaption>
-        <p class="text-white text-center md:text-start">I develop both both hybrid and native mobile application for
+        <figcaption class="text-white font-bold text-center text-lg md:text-xl">Mobile App Development</figcaption>
+        <p class="text-white text-center">I develop both both hybrid and native mobile application for
           android and ios</p>
       </figure>
     </div>
-    <div class="flex justify-start items-center h-52 px-2 sm:px-10 bg-gray-800 rounded">
-      <figure class="flex flex-col items-center md:items-start">
+    <div class="flex justify-center items-center h-52 px-2 sm:px-10 bg-gray-800 rounded">
+      <figure class="flex flex-col justify-center items-center">
         <desktop-icon class="w-9 text-white mb-4 md:mb-0" />
-        <figcaption class="text-white font-bold text-lg md:text-xl">Desktop App Development</figcaption>
-        <p class="text-white text-center md:text-start">I develop desktop applications for windows in Java</p>
+        <figcaption class="text-white font-bold text-center text-lg md:text-xl">Desktop App Development</figcaption>
+        <p class="text-white text-center">I develop desktop applications for windows in Java</p>
       </figure>
     </div>
   </div>
