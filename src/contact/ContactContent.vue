62087ab74531e78957ae37c8c25172be91337ed8
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2022-12-04 15:03:15 +0000

    modify the contact component and added one more breakpoint

--- src/contact/ContactContent.vue
+++ src/contact/ContactContent.vue
@@ -1,64 +1,58 @@
 <template>
     <div class="grid place-content-center bg-gray-200 py-8 truncate">
       <div class="mx-auto max-w-screen-xl container grid grid-cols-1 xs:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 px-8 md:px-5">
-        <a href="tel:+233256287192">
-          <span class="flex items-center">
-            <call-icon />
-            <span class="font-semibold">PHONE</span>
-          </span>
-          <span class="flex items-center">
-            <span class="text-sm font-medium">0256287192</span>
-            <open-icon />
-          </span>
-        </a>
         <a href="mailto: hackersgoddest@gmail.com">
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <mail-icon />
             <span class="font-semibold">EMAIL</span>
           </span>
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <span class="text-sm font-medium">hackersgoddest@gmail.com</span>
-            <open-icon />
+          </span>
+        </a>
+        <a href="tel:+233256287192">
+          <span class="flex justify-center">
+            <call-icon />
+            <span class="font-semibold">PHONE</span>
+          </span>
+          <span class="flex justify-center">
+            <span class="text-sm font-medium">0256287192</span>
           </span>
         </a>
         <a href="https://stackoverflow.com/users/17446079/frank-oppong-konadu" target="_blank">
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <stack-icon />
             <span class="font-semibold">STACKOVERFLOW</span>
           </span>
-          <div class="flex items-center">
+          <div class="flex justify-center">
             <span class="text-sm font-medium">Frank Oppong Konadu</span>
-            <open-icon />
           </div>
         </a>
         <a href="https://github.com/Hackersgoddest" target="_blank">
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <github-icon />
             <span class="font-semibold">GITHUB</span>
           </span>
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <span class="text-sm font-medium">Hackersgoddest</span>
-            <open-icon />
           </span>
         </a>
         <a href="https://www.linkedin.com/in/frank-oppong-konadu-9286b523b/" target="_blank">
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <linkedin-icon />
             <span class="font-semibold">LINKEDIN</span>
           </span>
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <span class="text-sm font-medium">Frank Oppong Konadu</span>
-            <open-icon />
           </span>
         </a>
         <a href="https://twitter.com/UndefineHacker" target="_blank">
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <twitter-icon />
             <span class="font-semibold">TWITTER</span>
           </span>
-          <span class="flex items-center">
+          <span class="flex justify-center">
             <span class="text-sm font-medium">Hackersgoddest</span>
-            <open-icon />
           </span>
         </a>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'xs': '500px',
+      'xs': '480px',
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
+      '2xl': '1536px',
     },
     extend: {},
   },
