383271d98ecdb11f5ca8d38b95485622cca87a13
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2022-12-04 06:50:40 +0000

    modify the footer

--- src/Footer.vue
+++ src/Footer.vue
@@ -3,7 +3,7 @@
       <marquee behavior="" direction="ltr" class="h-14 flex items-center">
         <div class="w-full flex  justify-start items-baseline">
             <span class="text-base font-semibold text-black mr-2">Oppong Frank Konadu</span>
-            <span class="text-sm font-medium flex items-baseline text-black"><span class="text-lg font-semibold">&copy;</span>2022. All Rights Reserved</span>
+            <span class="text-sm font-medium flex items-baseline text-black"><span class="text-lg mr-0.5 font-bold">&copy;</span><span>2022. All Rights Reserved</span></span>
           </div>
       </marquee>
     </footer>
