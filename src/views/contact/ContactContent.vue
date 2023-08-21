07784e2ecfdd2ec2e7e31fcb4a371ef788d799d4
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2023-08-21 20:43:44 +0000

    changed the text color on hover

--- src/views/contact/ContactContent.vue
+++ src/views/contact/ContactContent.vue
@@ -4,7 +4,7 @@
       class="mx-auto max-w-screen-xl container grid grid-cols-2 xs:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8 px-8 text-white md:px-5"
     >
       <template v-for="item in contactOptions" :key="item.name">
-        <a :href="item.link" target="_blank">
+        <a :href="item.link" target="_blank" class="hover:text-[#18A058] hover:transition-colors hover:duration-700 hover:ease-in-out">
           <span class="flex justify-center gap-1">
             <n-icon size="20" :component="item.icon" />
             <span class="font-semibold">{{ item.name }}</span>
@@ -78,12 +78,4 @@ const contactOptions = shallowRef([
 </script>
 
 <style scoped>
-div svg {
-  width: 20px;
-}
-
-a:hover {
-  color: #57e9b1;
-  transition: color 0.7s ease-in-out;
-}
 </style>
