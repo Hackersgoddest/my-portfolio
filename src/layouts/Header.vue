070b00978a8dd9bf909763245d71c4557c684f82
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2023-08-21 20:11:41 +0000

    changed text color

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -2,7 +2,7 @@
   <div
     class="flex flex-col justify-center items-center bg-[#101014] pt-20 pb-20"
   >
-    <n-h4 class="text-green-300 mb-1"> {{ title }} </n-h4>
+    <n-h4 class="text-[#18A058] mb-1"> {{ title }} </n-h4>
     <n-p class="text-xl font-normal mb-1 text-white">{{ description }}</n-p>
   </div>
 </template>
