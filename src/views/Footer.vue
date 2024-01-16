77fb0210649b1e4c8809f69c38a0ae5a402ba091
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2024-01-16 08:13:57 +0000

    generating the current year automatically instead doing it manually

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -3,7 +3,7 @@
       <!-- <marquee behavior="" direction="ltr" class="h-14 flex items-center"> -->
         <div class="w-full flex justify-center items-baseline">
             <span class="text-sm md:text-base font-semibold text-white mr-2">Oppong Frank Konadu</span>
-            <span class="text-xs md:text-sm relative font-medium flex items-baseline pl-4 text-white"><span class="text-base md:text-lg absolute left-0 bottom-[-4px] font-bold">&copy;</span><span>2022. All Rights Reserved</span></span>
+            <span class="text-xs md:text-sm relative font-medium flex items-baseline pl-4 text-white"><span class="text-base md:text-lg absolute left-0 bottom-[-4px] font-bold">&copy;</span><span>{{ new Date().getFullYear() }}. All Rights Reserved</span></span>
           </div>
       <!-- </marquee> -->
     </footer>
