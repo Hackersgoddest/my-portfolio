e1d526dc2124dfac6c364d5e2c971ffc71977f53
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2024-06-29 07:58:44 +0000

    use tag for my job description

--- src/views/Home/AuthorView.vue
+++ src/views/Home/AuthorView.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="container mx-auto max-w-screen-xl px-5">
     <div class="relative flex items-start justify-start flex-col gap-3">
-      <n-button
-        color="#fff"
-        dashed
-        size="small"
-        ghost
-        class="shadow-sm px-2 py-1 mb-1 hover:cursor-auto font-mono"
-      >
+      <!-- <n-button color="#fff" dashed size="small" ghost class="shadow-sm px-2 py-1 mb-1 hover:cursor-auto font-mono">
         Software Developer
-      </n-button>
-      <n-text class="text-white text-lg font-medium md:text-xl"
-        >Frank Oppong Konadu</n-text
-      >
-      <n-text class="text-white text-sm md:text-base"
-        >I am a passionate software developer with
+      </n-button> -->
+      <n-tag round :bordered="false" strong>
+        Software Developer
+        <template #icon>
+          <n-icon :component="Code" />
+        </template>
+      </n-tag>
+      <n-text class="text-white text-lg font-medium md:text-xl">Frank Oppong Konadu</n-text>
+      <n-text class="text-white text-sm md:text-base">I am a passionate software developer with
         {{ new Date().getFullYear() - 2022 }} years of experience
       </n-text>
       <span class="location__container">
@@ -24,12 +21,7 @@
         </div>
       </span>
       <n-space align="center" class="my-1">
-        <n-button
-          ghost
-          round
-          class="text-white"
-          @click.passive="$emit('scrollToSection')"
-        >
+        <n-button ghost round class="text-white" @click.passive="$emit('scrollToSection')">
           My Porfolio
         </n-button>
         <a href="https://drive.google.com/file/d/1vokKezr3mUj57Cze3UFYRU3keZvanRA7/view?usp=sharing" target="_blank">
@@ -37,14 +29,12 @@
             <template #icon>
               <n-icon size="14" v-html="skillsSvgs.Home[1].svg"></n-icon>
             </template>
-              My CV
+            My CV
           </n-button>
         </a>
       </n-space>
       <div class="flex order-5 mt-1">
-        <n-text class="mr-3 text-4xl text-white leading-9 order-0"
-          >{{ new Date().getFullYear() - 2022 }}+</n-text
-        >
+        <n-text class="mr-3 text-4xl text-white leading-9 order-0">{{ new Date().getFullYear() - 2022 }}+</n-text>
         <div class="flex flex-col justify-center">
           <n-text class="text-gray-400 text-xs">YEARS</n-text>
           <n-text class="text-gray-400 text-xs">EXPERIENCE</n-text>
@@ -55,8 +45,9 @@
 </template>
 
 <script setup>
-import { NText, NIcon, NButton, NSpace } from "naive-ui";
+import { NText, NIcon, NButton, NSpace, NTag } from "naive-ui";
 import skillsSvgs from "../../assets/svgs/svg";
+import { Code } from "@vicons/ionicons5"
 </script>
 
 <style scoped></style>
