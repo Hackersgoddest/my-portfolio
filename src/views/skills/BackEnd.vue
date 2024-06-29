30e0cc3e4d92c09291a4a7ff8a13a6d664698322
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2024-06-29 09:38:28 +0000

    add a component to display skills

--- src/views/skills/BackEnd.vue
+++ src/views/skills/BackEnd.vue
@@ -1,19 +1,10 @@
 <template>
-  <n-space justify="center" size="large" align="baseline">
-    <figure
-      class="flex flex-col justify-center items-center"
-      v-for="item in skillsSvgs.backEnd"
-      :key="item.name"
-    >
-      <n-text class="text-white">{{ item.name }}</n-text>
-      <n-icon size="40" v-html="item.svg"></n-icon>
-    </figure>
-  </n-space>
+  <DisplaySkill :skillName="skillsSvgs.backEnd" />
 </template>
 
 <script setup>
 import skillsSvgs from "../../assets/svgs/svg";
-import { NIcon, NSpace, NText } from "naive-ui";
+import DisplaySkill from "./DisplaySkill.vue";
 </script>
 
 <style scoped></style>
