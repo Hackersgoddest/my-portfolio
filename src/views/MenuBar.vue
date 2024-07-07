16138212ccacc69821b928c08e9937127d6b2281
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2024-07-07 08:00:41 +0000

    Replace old menu button with Naive UI NButton and NIcon components.

--- src/views/MenuBar.vue
+++ src/views/MenuBar.vue
@@ -3,59 +3,37 @@
     <div class="container flex w-full max-w-screen-xl px-5">
       <nav class="flex w-full justify-between h-16 items-center">
         <div class="flex items-center">
-          <n-gradient-text
-            gradient="linear-gradient(90deg, white 0%, gray 50%, green 100%)"
-            class="font-medium text-lg lg:text-xl"
-          >
+          <n-gradient-text gradient="linear-gradient(90deg, white 0%, gray 50%, green 100%)"
+            class="font-medium text-lg lg:text-xl">
             Frank Oppong Konadu
           </n-gradient-text>
         </div>
-        <ul
-          :class="[openMenu ? 'translate-x-0' : 'translate-x-[-100%]']"
-          class="block absolute left-0 top-16 bg-[#101014] w-full transition-transform pb-3 pl-2 pr-4 sm:px-0 sm:translate-x-0 sm:w-auto sm:static gap-2 sm:inline-flex sm:items-center sm:pb-0"
-        >
+        <ul :class="[openMenu ? 'translate-x-0' : 'translate-x-[-100%]']"
+          class="block absolute left-0 top-16 bg-[#101014] w-full transition-transform pb-3 pl-2 pr-4 sm:px-0 sm:translate-x-0 sm:w-auto sm:static gap-2 sm:inline-flex sm:items-center sm:pb-0">
           <li v-for="(section, index) in sections" :key="index">
-            <n-a
-              @click.passive="$emit('scrollToSection', section, index)"
+            <n-a @click.passive="$emit('scrollToSection', section, index)"
               class="hover:text-[#57e9b1] block px-3 py-1 mb-2 sm:mb-0 rounded-md text-sm font-medium transition-colors duration-500 ease-in-out"
               :class="{
-                'bg-[#18A0581F] text-[#57e9b1]': isActiveSection(index),
-              }"
-              >{{ section }}
+          'bg-[#18A0581F] text-[#57e9b1]': isActiveSection(index),
+        }">{{ section }}
             </n-a>
           </li>
         </ul>
       </nav>
     </div>
-    <button
-      @click="$emit('toggleMenu', !openMenu)"
-      class="absolute top-2.5 right-2 outline-none"
-    >
-      <template
-        v-show="!openMenu"
-        class="bar inline-flex items-center rounded-md px-1 py-2 hover:bg-[#18A0581F] z-10 sm:hidden w-10 flex-col justify-center transition-colors duration-1000 ease-in-out"
-      >
-        <div class="bg-gray-400 h-0.5 w-4 mb-1 rounded-xl"></div>
-        <div class="bg-gray-400 h-0.5 w-4 mb-1 rounded-xl"></div>
-        <div class="bg-gray-400 h-0.5 w-4 mb-0 rounded-xl"></div>
+    <n-button quaternary circle @click="$emit('toggleMenu', !openMenu)" class="absolute top-2.5 right-2 outline-none sm:hidden">
+      <template #icon class="z-10">
+        <n-icon v-if="!openMenu" color="#fff" size="30" :component="MenuSharp" />
+        <n-icon v-else size="30" color="#fff" :component="Close" />
       </template>
-      <template
-        v-show="openMenu"
-        class="bar inline-flex items-center rounded-md px-1 py-2 hover:bg-[#18A0581F] z-10 sm:hidden w-10 flex-col justify-center"
-      >
-        <div class="h-0.5 rounded-xl mb-1"></div>
-        <div
-          class="bg-gray-400 h-0.5 w-4 rounded-xl -rotate-45 translate-y-[1px]"
-        ></div>
-        <div class="bg-gray-400 h-0.5 w-4 mb-1 rounded-xl rotate-45"></div>
-      </template>
-    </button>
+    </n-button>
   </div>
 </template>
 
 <script setup>
 import { watch, onMounted, onUnmounted } from "vue";
-import { NA, NGradientText } from "naive-ui";
+import { NA, NGradientText, NButton, NIcon } from "naive-ui";
+import { Close, MenuSharp } from "@vicons/ionicons5";
 
 const props = defineProps(["openMenu", "activeSectionIndex"]);
 const emit = defineEmits(["updateActiveSectionIndex", "scrollToSection"]);
@@ -98,7 +76,4 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
-.bar:hover div {
-  background-color: #18a058;
-}
 </style>
