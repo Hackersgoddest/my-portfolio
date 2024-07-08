a2c47ed0c05682371e548010109878384325e18f
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2024-07-08 00:13:17 +0000

    updated navigation menu for mobile, tablet and desktop

--- src/views/MenuBar.vue
+++ src/views/MenuBar.vue
@@ -2,69 +2,113 @@
   <div class="bg-[#101014] flex justify-center fixed">
     <div class="container flex w-full max-w-screen-xl px-5">
       <nav class="flex w-full justify-between h-16 items-center">
-        <div class="flex items-center">
+        <div class="flex items-center flex-1">
           <n-gradient-text gradient="linear-gradient(90deg, white 0%, gray 50%, green 100%)"
             class="font-medium text-lg lg:text-xl">
             Frank Oppong Konadu
           </n-gradient-text>
         </div>
-        <ul :class="[openMenu ? 'translate-x-0' : 'translate-x-[-100%]']"
-          class="block absolute left-0 top-16 bg-[#101014] w-full transition-transform pb-3 pl-2 pr-4 sm:px-0 sm:translate-x-0 sm:w-auto sm:static gap-2 sm:inline-flex sm:items-center sm:pb-0">
-          <li v-for="(section, index) in sections" :key="index">
-            <n-a @click.passive="$emit('scrollToSection', section, index)"
-              class="hover:text-[#57e9b1] block px-3 py-1 mb-2 sm:mb-0 rounded-md text-sm font-medium transition-colors duration-500 ease-in-out"
-              :class="{
-          'bg-[#18A0581F] text-[#57e9b1]': isActiveSection(index),
-        }">{{ section }}
-            </n-a>
-          </li>
-        </ul>
+        <!-- For Desktop -->
+        <div class="justify-end items-center hidden lg:flex">
+          <n-menu v-model:value="activeMenuKey" mode="horizontal" :options="menuOptions" responsive
+            :theme-overrides="darkTheme" :on-update:value="(key, item) => updateActiveMenuKey(key)" />
+        </div>
+        <!-- For Tablet -->
+        <div class="justify-end items-center hidden md:flex lg:hidden">
+          <n-menu v-model:value="activeMenuKey" mode="horizontal" :options="mdMenuOptions" responsive
+            :theme-overrides="darkTheme" :on-update:value="(key, item) => updateActiveMenuKey(key)" />
+        </div>
+        <!-- For Mobile -->
+        <div :class="[openMenu ? 'translate-x-0' : 'translate-x-[-100%]']"
+          class="block absolute left-0 top-16 bg-[#101014] w-full transition-transform md:hidden">
+          <n-menu v-model:value="activeMenuKey" mode="vertical" :options="menuOptions" responsive
+            :theme-overrides="darkTheme" :on-update:value="(key, item) => updateActiveMenuKey(key)" />
+        </div>
       </nav>
     </div>
-    <n-button quaternary circle @click="$emit('toggleMenu', !openMenu)" class="absolute top-2.5 right-2 outline-none sm:hidden">
+    <n-button strong secondary circle color="#a3a3a3" @click="openMenu = !openMenu"
+      class="absolute top-2.5 right-2 outline-none md:hidden">
       <template #icon class="z-10">
-        <n-icon v-if="!openMenu" color="#fff" size="30" :component="MenuSharp" />
-        <n-icon v-else size="30" color="#fff" :component="Close" />
+        <n-icon v-if="!openMenu" size="24" :component="MenuSharp" />
+        <n-icon v-else size="24" :component="Close" />
       </template>
     </n-button>
   </div>
 </template>
 
 <script setup>
-import { watch, onMounted, onUnmounted } from "vue";
-import { NA, NGradientText, NButton, NIcon } from "naive-ui";
-import { Close, MenuSharp } from "@vicons/ionicons5";
+import { darkTheme } from 'naive-ui'
+import { onMounted, onUnmounted, ref, h } from "vue";
+import { NGradientText, NButton, NIcon, NMenu } from "naive-ui";
+import { Close, MenuSharp, HomeOutline, FolderOutline, LinkOutline, BriefcaseOutline, CodeOutline } from "@vicons/ionicons5";
 
-const props = defineProps(["openMenu", "activeSectionIndex"]);
-const emit = defineEmits(["updateActiveSectionIndex", "scrollToSection"]);
+const emit = defineEmits(["scrollToSection"]);
 
-const sections = ["Home", "Services", "Skills", "Portfolio", "Contact"];
+const activeMenuKey = ref("home");
+const openMenu = ref(false);
+
+function updateActiveMenuKey(key) {
+  activeMenuKey.value = key;
+  openMenu.value = false;
+  emit('scrollToSection', key)
+}
+
+function renderIcon(icon) {
+  return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+const menuOptions = [
+  {
+    label: "Home",
+    key: "home",
+    icon: renderIcon(HomeOutline),
+  },
+  {
+    label: "Services",
+    key: "services",
+    icon: renderIcon(BriefcaseOutline),
+  },
+  {
+    label: "Skills",
+    key: "skills",
+    icon: renderIcon(CodeOutline)
+  },
+  {
+    label: "Portfolio",
+    key: "portfolio",
+    icon: renderIcon(FolderOutline)
+  },
+  {
+    label: "Contact",
+    key: "contact",
+    icon: renderIcon(LinkOutline)
+  },
+];
+
+const mdMenuOptions = menuOptions.map((option) => ({
+  label: option.label,
+  key: option.key,
+}))
 
 const handleScroll = () => {
-  const sectionRefs = sections.map((_, index) =>
-    document.getElementById(sections[index])
+  const sectionRefs = menuOptions.map((_, index) =>
+    document.getElementById(menuOptions[index].label)
   );
-  for (let i = sectionRefs.length - 1; i >= 0; i--) {
-    const rect = sectionRefs[i].getBoundingClientRect();
-    if (rect.top < window.innerHeight * 0.5) {
-      emit("updateActiveSectionIndex", i);
-      break;
+  for (let i = sectionRefs.length - 1; i >= -1; i--) {
+    const section = sectionRefs[i];
+    if (section) {
+      const rect = section.getBoundingClientRect();
+      if (rect.top < window.innerHeight * 0.5) {
+        activeMenuKey.value = menuOptions[i].key;
+        break;
+      }
     }
   }
 };
 
-watch(
-  () => props.activeSectionIndex,
-  () => {
-    isActiveSection(props.activeSectionIndex);
-  }
-);
-
-const isActiveSection = (index) => {
-  return props.activeSectionIndex === index;
-};
 
 onMounted(() => {
+  handleScroll();
   window.addEventListener("wheel", handleScroll, { passive: true });
   window.addEventListener("touchmove", handleScroll, { passive: true });
 });
@@ -75,5 +119,4 @@ onUnmounted(() => {
 });
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
