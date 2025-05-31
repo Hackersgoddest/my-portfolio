0dda374748558392ee57184005df8406164cfa23
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2025-05-31 07:31:12 +0000

    refactor: replace n-flex with n-grid for improved layout in DisplaySkill component

--- src/components/DisplaySkill.vue
+++ src/components/DisplaySkill.vue
@@ -1,38 +1,52 @@
 <template>
-    <n-flex justify="center" size="large" align="baseline">
-        <div class="lg:hidden flex gap-4 flex-wrap justify-center items-center px-2">
-            <div v-for="item in skillName" :key="item.name" class="flex flex-col justify-center items-center">
-                <n-tag :bordered="false" strong>
-                    {{ item.name }}
-                    <template #icon>
-                        <n-icon v-html="item.svg"></n-icon>
-                    </template>
-                </n-tag>
-            </div>
-        </div>
-        <div class="hidden lg:flex gap-8 flex-wrap justify-center items-center">
-            <div v-for="item in skillName" :key="item.name" class="flex">
-                <n-tooltip trigger="hover" :raw="true" :show-arrow="false">
-                    <template #trigger>
-                        <n-button quaternary circle>
-                            <template #icon>
-                                <n-icon size="40" v-html="item.svg"></n-icon>
-                            </template>
-                        </n-button>
-                    </template>
-                    <n-tag :bordered="false" strong>
-                        {{ item.name }}
-                        <template #icon>
-                            <n-icon v-html="item.svg"></n-icon>
-                        </template>
-                    </n-tag>
-                </n-tooltip>
-            </div>
-        </div>
-    </n-flex>
+  <div class="grid lg:hidden">
+    <n-grid
+      cols="2 s:3 m:4"
+      responsive="screen"
+      :x-gap="32"
+      :y-gap="32"
+      :item-responsive="true"
+    >
+      <n-grid-item v-for="item in skillName" :key="item.name">
+        <n-tag :bordered="false" strong>
+          {{ item.name }}
+          <template #icon>
+            <n-icon v-html="item.svg"></n-icon>
+          </template>
+        </n-tag>
+      </n-grid-item>
+    </n-grid>
+  </div>
+  <div class="hidden lg:grid">
+    <n-grid
+      cols="8 l:10 xl:12 2xl:14"
+      responsive="screen"
+      :x-gap="32"
+      :y-gap="32"
+      :item-responsive="true"
+    >
+      <n-grid-item v-for="item in skillName" :key="item.name">
+        <n-tooltip trigger="hover" :raw="true" :show-arrow="false">
+          <template #trigger>
+            <n-button quaternary circle>
+              <template #icon>
+                <n-icon size="40" v-html="item.svg"></n-icon>
+              </template>
+            </n-button>
+          </template>
+          <n-tag :bordered="false" strong>
+            {{ item.name }}
+            <template #icon>
+              <n-icon v-html="item.svg"></n-icon>
+            </template>
+          </n-tag>
+        </n-tooltip>
+      </n-grid-item>
+    </n-grid>
+  </div>
 </template>
 <script setup>
-import { NIcon, NFlex, NText, NTooltip, NButton, NTag } from "naive-ui";
+import { NIcon,NGrid, NGridItem, NTooltip, NButton, NTag } from "naive-ui";
 
 defineProps(["skillName"]);
 </script>
