039f280ec4c8c3707f7a86e2d07489f2b3eb0a81
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2025-05-31 08:10:46 +0000

    refactor: replace grid with flex layout for better responsiveness in DisplaySkill component

--- src/components/DisplaySkill.vue
+++ src/components/DisplaySkill.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="grid lg:hidden">
-    <n-grid
-      cols="2 s:3 m:4"
-      responsive="screen"
-      :x-gap="32"
-      :y-gap="32"
-      :item-responsive="true"
-    >
-      <n-grid-item v-for="item in skillName" :key="item.name">
-        <n-tag :bordered="false" strong>
-          {{ item.name }}
-          <template #icon>
-            <n-icon v-html="item.svg"></n-icon>
-          </template>
-        </n-tag>
-      </n-grid-item>
-    </n-grid>
+  <div class="lg:hidden">
+    <n-flex justify="center" size="large" align="baseline">
+      <n-tag
+        v-for="item in skillName"
+        :key="item.name"
+        :bordered="false"
+        strong
+      >
+        {{ item.name }}
+        <template #icon>
+          <n-icon v-html="item.svg"></n-icon>
+        </template>
+      </n-tag>
+    </n-flex>
   </div>
   <div class="hidden lg:grid">
     <n-grid
@@ -46,7 +43,15 @@
   </div>
 </template>
 <script setup>
-import { NIcon,NGrid, NGridItem, NTooltip, NButton, NTag } from "naive-ui";
+import {
+  NIcon,
+  NGrid,
+  NGridItem,
+  NTooltip,
+  NButton,
+  NTag,
+  NFlex,
+} from "naive-ui";
 
 defineProps(["skillName"]);
 </script>
