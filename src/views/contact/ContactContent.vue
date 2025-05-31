6ddb611c778d2aad6b62bea44c0a02303a5109f5
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2025-05-31 07:32:52 +0000

    refactor: replace div with n-grid for improved layout in ContactContent component

--- src/views/contact/ContactContent.vue
+++ src/views/contact/ContactContent.vue
@@ -1,18 +1,28 @@
 <template>
-    <div class="max-w-screen-xl mx-auto flex flex-wrap gap-8 xl:gap-12 justify-center pb-20">
-      <template v-for="item in contactOptions" :key="item.name">
-        <a :href="item.link" target="_blank"
-          class="text-white hover:text-[#18A058] hover:transition-colors hover:duration-700 hover:ease-in-out">
-          <span class="flex justify-center gap-1">
-            <n-icon size="20" :component="item.icon" />
-            <span class="font-semibold">{{ item.name }}</span>
-          </span>
-          <span class="flex justify-center">
-            <span class="text-sm font-medium">{{ item.description }}</span>
-          </span>
-        </a>
-      </template>
-    </div>
+  <n-grid
+    class="max-w-screen-xl mx-auto px-5"
+    cols="2 s:3 m:4 l:5 xl:6 2xl:7"
+    responsive="screen"
+    :x-gap="32"
+    :y-gap="32"
+    :item-responsive="true"
+  >
+    <n-grid-item v-for="item in contactOptions" :key="item.name">
+      <a
+        :href="item.link"
+        target="_blank"
+        class="text-white hover:text-[#18A058] hover:transition-colors hover:duration-700 hover:ease-in-out"
+      >
+        <span class="flex justify-center gap-1">
+          <n-icon size="20" :component="item.icon" />
+          <span class="font-semibold">{{ item.name }}</span>
+        </span>
+        <span class="flex justify-center">
+          <span class="text-sm font-medium">{{ item.description }}</span>
+        </span>
+      </a>
+    </n-grid-item>
+  </n-grid>
 </template>
 
 <script setup>
@@ -25,7 +35,7 @@ import {
   LogoTwitter as TwitterIcon,
   LogoInstagram as InstagramIcon,
 } from "@vicons/ionicons5";
-import { NIcon } from "naive-ui";
+import { NIcon, NGrid, NGridItem } from "naive-ui";
 import { shallowRef } from "vue";
 
 const contactOptions = shallowRef([
