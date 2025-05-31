e912c7a2f3f59fd7b28186eb1c426415b8d458c0
Frank Oppong Konadu <frankoppongkonadu930@gmail.com>
2025-05-31 07:31:47 +0000

    refactor: adjust padding in Header component for improved layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-screen-xl mx-auto flex flex-col items-center pt-20 pb-20">
+  <div class="max-w-screen-xl mx-auto flex flex-col items-center pb-10 px-5">
     <n-h4 class="text-[#18A058] mb-1"> {{ title }} </n-h4>
     <n-p class="text-xl font-normal mb-1 text-white">{{ description }}</n-p>
   </div>
